77d636f1ae53adcf0b58a8ecf25cb48830801ed5
Tirell Mckinnon <tdmckinn@gmail.com>
2017-02-23 17:22:25 -0500

    style cleaning switch text color

--- presentation/index.js
+++ presentation/index.js
@@ -42,7 +42,7 @@ const theme = createTheme({
   secondary: "Helvetica"
 })
 
-require('./z60.css')
+require("./z60.css")
 
 export default class Presentation extends React.Component {
   render() {
@@ -50,11 +50,11 @@ export default class Presentation extends React.Component {
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            <Text margin="10px 0 0" textColor="malibu" size={1} bold>
+            <Text margin="10px 0 0" textColor="denim" size={1} bold>
               Zero to Sixty with
             </Text>
             <Image src={images.webpacklogo} height="175px"/>
-            <Text margin="0 0 0" textColor="malibu" size={1} bold>
+            <Text margin="0 0 0" textColor="denim" size={1} bold>
              Two
             </Text>
           </Heading>
@@ -62,7 +62,7 @@ export default class Presentation extends React.Component {
 
         <Slide transition={["fade"]} bgColor="primary" >
           <BlockQuote>
-            <Text textColor="malibu">Simple made easy</Text>
+            <Text textColor="denim">Simple made easy</Text>
             <Quote className="intro-quote" textColor="secondary">"Simplicity is hard work. But, there's a huge payoff. The person who has a genuinely simpler system - a system made out of genuinely simple parts,
               is going to be able to affect the greatest change with the least work."</Quote>
             <Cite>@Rick Hickey </Cite>
@@ -70,7 +70,7 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary">
-          <Heading size={6} textColor="malibu" caps>Take Aways</Heading>
+          <Heading size={6} textColor="denim" caps>Take Aways</Heading>
           <List>
             <ListItem>Reasons for using webpack</ListItem>
             <ListItem>wepback key principals </ListItem>
@@ -82,61 +82,61 @@ export default class Presentation extends React.Component {
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary">What is webpack?</Heading>
-          
+
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary">Why webpack?</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary">webpack key principals</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Entry</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Output</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Loaders</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Plugins</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Advanced</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Performance</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Base, Dev, Prod, wepback configs</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Resources</Heading>
-          
+
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Thank you</Heading>
-          
+
         </Slide>
       </Deck>
     )
